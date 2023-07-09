94f5252d607c821f3bd70a22c48fdbffb43373b1
curreesan <curreesan17@gmail.com>
2023-07-09 17:07:25 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '86af9eb8-34ef-4538-a011-4906b41b5532';
+export const MODAL_CLOSE_SEC = 2.5;
